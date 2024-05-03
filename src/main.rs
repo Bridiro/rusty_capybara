178a6905ac0e415f2779bdd7dc062f0e68307230
Bridiro <ale.bridi15@gmail.com>
2024-05-03 17:36:07 +0200

    visual anche non grafica con thread migliorato e canale di comunicazione

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,24 @@ use std::thread;
 use crate::map::Maze;
 use crate::sensors::mpu6050::MPU6050;
 use crate::sensors::vl6180x::VL6180X;
-use crate::vision::Vision;
+use crate::vision::{Detection, Vision};
+use std::sync::mpsc::channel;
 
 fn main() {
     let camera_index = 0;
     let model_path = "bestsmall.onnx";
-    let classes_labels: Vec<&str> = vec!["GREEN", "H", "RED", "S", "U", "YELLOW"];
+    let classes_labels: Vec<String> = vec![
+        String::from("GREEN"),
+        String::from("H"),
+        String::from("RED"),
+        String::from("S"),
+        String::from("U"),
+        String::from("YELLOW"),
+    ];
     let net_width = 480;
     let net_height = 384;
     let class_filters: Vec<usize> = vec![];
+    let (detection_channel, result_channel) = channel::<Detection>();
     if let Ok(mut vis) = Vision::new(
         camera_index,
         model_path,
@@ -22,14 +31,19 @@ fn main() {
         net_width,
         net_height,
         class_filters,
+        detection_channel,
     ) {
-        thread::spawn(move || {
-            if let Ok(()) = vis.run(0.6, 0.7) {
-                println!("Done!");
-            } else {
-                println!("Error running object detection!");
+        if let Ok(()) = vis.run(0.6, 0.7, false) {
+            for _ in 0..200 {
+                if let Ok(detection) = result_channel.recv() {
+                    println!(
+                        "Class: {}  Confidence: {}  BBox: {:?}",
+                        detection.class_label, detection.confidence, detection.bbox
+                    );
+                }
+                thread::sleep(std::time::Duration::from_millis(300));
             }
-        });
+        }
     } else {
         println!("Error creating object!");
     }
--- src/vision.rs
+++ src/vision.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code)]
+use od_opencv::{model_format::ModelFormat, model_ultralytics::ModelUltralyticsV8};
 use opencv::{
-    core::{Point, Scalar, Size},
+    core::{Point, Rect, Scalar, Size},
     dnn::{DNN_BACKEND_OPENCV, DNN_TARGET_CPU}, // I will utilize my GPU to perform faster inference. Your way may vary
     highgui,
     imgproc::{self, put_text, rectangle, FONT_HERSHEY_SIMPLEX, LINE_4},
@@ -7,32 +9,44 @@ use opencv::{
     videoio,
     Result,
 };
+use std::sync::mpsc::Sender;
+use std::sync::{Arc, Mutex};
 
-use od_opencv::{model_format::ModelFormat, model_ultralytics::ModelUltralyticsV8};
-
-pub struct Vision<'a> {
-    cam: videoio::VideoCapture,
-    model: ModelUltralyticsV8,
-    classes_labels: Vec<&'a str>,
+pub struct Vision {
+    cam: Arc<Mutex<videoio::VideoCapture>>,
+    model: Arc<Mutex<ModelUltralyticsV8>>,
+    classes_labels: Vec<String>,
     net_width: i32,
     net_height: i32,
+    detection_channel: Sender<Detection>,
+    running: Arc<Mutex<bool>>,
 }
 
-impl<'a> Vision<'a> {
+pub struct Detection {
+    pub class_label: String,
+    pub confidence: f32,
+    pub bbox: Rect,
+}
+
+impl Vision {
     pub fn new(
         camera_index: i32,
         model_path: &str,
-        classes_labels: Vec<&'a str>,
+        classes_labels: Vec<String>,
         net_width: i32,
         net_height: i32,
         class_filters: Vec<usize>,
+        detection_channel: Sender<Detection>,
     ) -> Result<Self> {
-        let cam = videoio::VideoCapture::new(camera_index, videoio::CAP_ANY)?;
-        if !videoio::VideoCapture::is_opened(&cam)? {
+        let cam = Arc::new(Mutex::new(videoio::VideoCapture::new(
+            camera_index,
+            videoio::CAP_ANY,
+        )?));
+        if !videoio::VideoCapture::is_opened(&cam.lock().unwrap())? {
             panic!("Unable to open default camera!");
         }
         let mf = ModelFormat::ONNX;
-        let model = ModelUltralyticsV8::new_from_file(
+        let model = Arc::new(Mutex::new(ModelUltralyticsV8::new_from_file(
             model_path,
             None,
             (net_width, net_height),
@@ -40,70 +54,106 @@ impl<'a> Vision<'a> {
             DNN_BACKEND_OPENCV,
             DNN_TARGET_CPU,
             class_filters.clone(),
-        )?;
+        )?));
+        let running = Arc::new(Mutex::new(false));
         Ok(Self {
             cam,
             model,
             classes_labels,
             net_width,
             net_height,
+            detection_channel,
+            running,
         })
     }
 
-    pub fn run(&mut self, conf_threshold: f32, nms_threshold: f32) -> Result<()> {
-        let window = "video capture";
-        highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
-        loop {
-            let mut frame = Mat::default();
-            self.cam.read(&mut frame)?;
+    pub fn run(&mut self, conf_threshold: f32, nms_threshold: f32, graphical: bool) -> Result<()> {
+        *self.running.lock().unwrap() = true;
+        let running = self.running.clone();
+        let detection_channel = self.detection_channel.clone();
+        let classes_labels = self.classes_labels.clone();
+        let net_width = self.net_width.clone();
+        let net_height = self.net_height.clone();
+        let cam = self.cam.clone();
+        let model = self.model.clone();
 
-            let mut resized = Mat::default();
-            imgproc::resize(
-                &frame,
-                &mut resized,
-                Size {
-                    width: self.net_width,
-                    height: self.net_height,
-                },
-                0.0,
-                0.0,
-                imgproc::INTER_AREA,
-            )?;
+        std::thread::spawn(move || -> Result<()> {
+            let window = "video capture";
+            if graphical {
+                highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+            }
+            while *running.lock().unwrap() {
+                let mut frame = Mat::default();
+                cam.lock().unwrap().read(&mut frame)?;
 
-            let (bboxes, class_ids, confidences) =
-                self.model
-                    .forward(&resized, conf_threshold, nms_threshold)?;
-            for (i, bbox) in bboxes.iter().enumerate() {
-                let class_label = self.classes_labels[class_ids[i]];
-                let confidence_text = format!("{:.2}", confidences[i]);
-                rectangle(
+                let mut resized = Mat::default();
+                imgproc::resize(
+                    &frame,
                     &mut resized,
-                    *bbox,
-                    Scalar::new(0.0, 255.0, 0.0, 0.0),
-                    2,
-                    LINE_4,
-                    0,
-                )?;
-                put_text(
-                    &mut resized,
-                    &format!("{}: {}", class_label, confidence_text),
-                    Point {
-                        x: bbox.x,
-                        y: bbox.y,
+                    Size {
+                        width: net_width,
+                        height: net_height,
                     },
-                    FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    Scalar::new(0.0, 255.0, 0.0, 0.0),
-                    1,
-                    LINE_4,
-                    false,
+                    0.0,
+                    0.0,
+                    imgproc::INTER_AREA,
                 )?;
+
+                let (bboxes, class_ids, confidences) =
+                    model
+                        .lock()
+                        .unwrap()
+                        .forward(&resized, conf_threshold, nms_threshold)?;
+
+                for (i, bbox) in bboxes.iter().enumerate() {
+                    let class_label = &classes_labels[class_ids[i]];
+                    let confidence_text = format!("{:.2}", confidences[i]);
+                    let detection = Detection {
+                        class_label: class_label.to_string(),
+                        confidence: confidences[i],
+                        bbox: *bbox,
+                    };
+                    detection_channel
+                        .send(detection)
+                        .map_err(|err| opencv::Error::new(0, err.to_string()))?;
+                    if graphical {
+                        rectangle(
+                            &mut resized,
+                            *bbox,
+                            Scalar::new(0.0, 255.0, 0.0, 0.0),
+                            2,
+                            LINE_4,
+                            0,
+                        )?;
+                        put_text(
+                            &mut resized,
+                            &format!("{}: {}", class_label, confidence_text),
+                            Point {
+                                x: bbox.x,
+                                y: bbox.y,
+                            },
+                            FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            Scalar::new(0.0, 255.0, 0.0, 0.0),
+                            1,
+                            LINE_4,
+                            false,
+                        )?;
+                    }
+                }
+                if graphical {
+                    highgui::imshow(window, &resized)?;
+                    if highgui::wait_key(10)? > 0 {
+                        break;
+                    }
+                }
             }
-            highgui::imshow(window, &resized)?;
-            if highgui::wait_key(10)? > 0 {
-                break;
-            }
-        }
+            Ok(())
+        });
         Ok(())
+    }
+
+    pub fn stop(&self) {
+        *self.running.lock().unwrap() = false;
     }
 }
